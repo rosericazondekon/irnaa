31bdb141fe7fcdf7fd7def92a43dc69b882a7676
Roseric Azondekon <roseric_2000@yahoo.fr>
2019-05-09 17:31:19 -0500

    Last changes as of 05/09/2019

--- linux/ui/globalrcUI.R
+++ linux/ui/globalrcUI.R
@@ -14,24 +14,29 @@ DESeq2_grp <- tabItem(tabName = "DESeq2_grp", br(), br(),
                                ,status = "primary", solidHeader = TRUE
                                ,plotlyOutput("DESeq2_pca", width = "75%", height = "600px")#,plotOutput("DESeq2_pca")
                       )
-                      # ,boxPlus(collapsible=T, closable=F, width = 10, title = "Pairwise Correlation"
-                      #          # ,loadingState()
-                      #          ,status = "primary", solidHeader = TRUE
-                      #          ,dataTableOutput("DESeq2_pcorr", height="500px"),br()
-                      #          ,uiOutput("DESeq2_pcorr_dld")
-                      # )
-                      ,fluidRow(column(10,flipBox(id=2,main_img = "corr.svg"
-                                                  ,header_img = "https://image.flaticon.com/icons/svg/119/119596.svg"
-                                                  ,front_title = "Pairwise Correlation Table"
-                                                  ,back_title = "Correlation matrix"
-                                                  ,front_btn_text = "View Correlation Matrix"
-                                                  ,back_btn_text = "Back to Correlation Table", width = 10
-                                                  ,tagList(dataTableOutput("DESeq2_pcorr", height="500px",width="99%"),br()
-                                                    ,uiOutput("DESeq2_pcorr_dld")
-                                                  )
-                                                  ,back_content = tagList(
-                                                    plotlyOutput("pcorr_mat", height = '500px')
-                                                  )
-                      ))
+                      ,boxPlus(collapsible=T, closable=F, width = 10, title = "Pairwise Correlation"
+                               # ,loadingState()
+                               ,status = "primary", solidHeader = TRUE
+                               ,dataTableOutput("DESeq2_pcorr", height="500px"),br()
+                               ,uiOutput("DESeq2_pcorr_dld")
                       )
+                      ,boxPlus(collapsible=T, closable=F, width = 10, title = "Correlation Heatmap"
+                               # ,loadingState()
+                               ,status = "primary", solidHeader = TRUE
+                               ,plotlyOutput("pcorr_mat", height = '500px')
+                      )
+                      # ,fluidRow(column(10,flipBox(id=2,main_img = "corr.svg"
+                      #                             ,header_img = "https://image.flaticon.com/icons/svg/119/119596.svg"
+                      #                             ,front_title = "Pairwise Correlation Table"
+                      #                             ,back_title = "Correlation matrix"
+                      #                             ,front_btn_text = "View Correlation Matrix"
+                      #                             ,back_btn_text = "Back to Correlation Table", width = 10
+                      #                             ,tagList(dataTableOutput("DESeq2_pcorr", height="500px",width="99%"),br()
+                      #                               ,uiOutput("DESeq2_pcorr_dld")
+                      #                             )
+                      #                             ,back_content = tagList(
+                      #                               plotlyOutput("pcorr_mat", height = '500px')
+                      #                             )
+                      # ))
+                      # )
 )
