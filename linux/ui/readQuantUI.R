74c2cb153a9de747e1adc7996cc5889ebd256957
Roseric Azondekon <roseric_2000@yahoo.fr>
2019-05-10 14:15:49 -0500

    Some minor box title changes

--- linux/ui/readQuantUI.R
+++ linux/ui/readQuantUI.R
@@ -1,5 +1,5 @@
 ##readQuant: read Quantification
-readQuant <- tabItem(tabName = "readQuant", br(), br(), 
+readQuant <- tabItem(tabName = "readQuant", br(), br(),
                      h2("Read Quantification"), br()
                      # ,radioButtons("quantType", h3(""),list("From FASTQ files"="fqc","From TCGA"="tcga","From files"="files"), selected = "fqc", inline=T)
                      ,radioGroupButtons(inputId = "quantType"
@@ -51,24 +51,24 @@ readQuant <- tabItem(tabName = "readQuant", br(), br(),
                                                 ,boxPlus(collapsible=T, closable=F, width = 10, title = "Export TCGA data"
                                                          ,status = "primary", solidHeader = TRUE
                                                          ,fluidRow(column(6,selectInput("disease_select", "Select a table"
-                                                                                        ,list("adrenocortical carcinoma (ACC)"="ACC", "bladder urothelial carcinom (BLCA)"="BLCA", 
+                                                                                        ,list("adrenocortical carcinoma (ACC)"="ACC", "bladder urothelial carcinom (BLCA)"="BLCA",
                                                                                               "breast invasive carcinoma (BRCA)"="BRCA", "cervical and endocervical cancers (CESC)"="CESC",
-                                                                                              "cholangiocarcinoma (CHOL)"="CHOL", "colon adenocarcinoma (COAD)"="COAD", 
-                                                                                              "colorectal adenocarcinoma (COADREAD)"="COADREAD", "large B-cell lymphoma (DLBC)"="DLBC", 
-                                                                                              "esophageal carcinoma (ESCA)"="ESCA", "FFPE Pilot Phase II (FPPP)"="FPPP", 
+                                                                                              "cholangiocarcinoma (CHOL)"="CHOL", "colon adenocarcinoma (COAD)"="COAD",
+                                                                                              "colorectal adenocarcinoma (COADREAD)"="COADREAD", "large B-cell lymphoma (DLBC)"="DLBC",
+                                                                                              "esophageal carcinoma (ESCA)"="ESCA", "FFPE Pilot Phase II (FPPP)"="FPPP",
                                                                                               "glioblastoma multiforme (GBM)"="GBM", "glioma (GBMLGG)"="GBMLGG",
-                                                                                              "head and neck squamous cell carcinoma (HNSC)"="HNSC", "kidney chromophobe (KICH)"="KICH", 
+                                                                                              "head and neck squamous cell carcinoma (HNSC)"="HNSC", "kidney chromophobe (KICH)"="KICH",
                                                                                               "pan-kidney cohort (KIPAN = KICH+KIRC+KIRP)"="KIPAN", "kidney renal clear cell carcinoma (KIRC)"="KIRC",
-                                                                                              "kidney renal papillary cell carcinoma (KIRP)"="KIRP", "acute myeloid leukemia (LAML)"="LAML", 
-                                                                                              "brain lower grade glioma (LGG)"="LGG", "liver hepatocellular carcinoma (LIHC)"="LIHC", 
-                                                                                              "lung adenocarcinoma (LUAD)"="LUAD", "lung squamous cell carcinoma (LUSC)"="LUSC", 
-                                                                                              "mesothelioma (MESO)"="MESO", "ovarian serous cystadenocarcinoma (OV)"="OV", 
+                                                                                              "kidney renal papillary cell carcinoma (KIRP)"="KIRP", "acute myeloid leukemia (LAML)"="LAML",
+                                                                                              "brain lower grade glioma (LGG)"="LGG", "liver hepatocellular carcinoma (LIHC)"="LIHC",
+                                                                                              "lung adenocarcinoma (LUAD)"="LUAD", "lung squamous cell carcinoma (LUSC)"="LUSC",
+                                                                                              "mesothelioma (MESO)"="MESO", "ovarian serous cystadenocarcinoma (OV)"="OV",
                                                                                               "pancreatic adenocarcinoma (PAAD)"="PAAD", "pheochromocytoma and paraganglioma (PCPG)"="PCPG",
-                                                                                              "prostate adenocarcinoma (PRAD)"="PRAD", "rectum adenocarcinoma (READ)"="READ", 
+                                                                                              "prostate adenocarcinoma (PRAD)"="PRAD", "rectum adenocarcinoma (READ)"="READ",
                                                                                               "sarcoma (SARC)"="SARC", "skin cutaneous melanoma (SKCM)"="SKCM", "stomach adenocarcinoma (STAD)"="STAD",
                                                                                               "testicular germ cell tumors (TGCT)"="TGCT", "thyroid carcinoma (THCA)"="THCA",
-                                                                                              "thymoma (THYM)"="THYM", "uterine corpus endometrial carcinoma (UCEC)"="UCEC", 
-                                                                                              "uterine carcinosarcoma (UCS)"="UCS", "uveal melanoma (UVM)"="UVM"), 
+                                                                                              "thymoma (THYM)"="THYM", "uterine corpus endometrial carcinoma (UCEC)"="UCEC",
+                                                                                              "uterine carcinosarcoma (UCS)"="UCS", "uveal melanoma (UVM)"="UVM"),
                                                                                         selected = "OV", multiple = FALSE,
                                                                                         selectize = TRUE, width = "400px", size = NULL))
                                                                    ,column(4,selectInput("dataType", "Select a data type"
@@ -85,7 +85,7 @@ readQuant <- tabItem(tabName = "readQuant", br(), br(),
                                        )
                      )
                      ,conditionalPanel(condition = "input.quantType == 'files'"
-                                       ,boxPlus(collapsible=T, closable=F, width = 10, title = "Load RNASeq data from file"
+                                       ,boxPlus(collapsible=T, closable=F, width = 10, title = "Load read counts dataset from file"
                                                 ,status = "primary", solidHeader = T
                                                 ,fileInput('datafile', 'Load a dataset (.xlsx, .xls, .txt or .csv)',
                                                            accept=c('text/csv','text/comma-separated-values,text/plain',".xls",".xlsx",".csv")
